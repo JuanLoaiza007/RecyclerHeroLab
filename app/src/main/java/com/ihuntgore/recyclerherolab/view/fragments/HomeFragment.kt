11a21c27d620679e37c01880ca22d1e6075d36b0
JuanLoaiza007 <juan.loaiza.santiago@correounivalle.edu.co>
2024-10-11 08:53:22 -0500

    feat: create and clean HomeFragment with dataBinding settings

---
+++ app/src/main/java/com/ihuntgore/recyclerherolab/view/fragments/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.ihuntgore.recyclerherolab.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ihuntgore.recyclerherolab.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+}
