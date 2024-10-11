5f726277d515eea76da87c1c94bc4ad65c54a337
JuanLoaiza007 <juan.loaiza.santiago@correounivalle.edu.co>
2024-10-11 08:55:49 -0500

    feat: create and clean HomeFragment with dataBinding settings - (STEP CORRECTION)

--- app/src/main/java/com/ihuntgore/recyclerherolab/view/fragments/HomeFragment.kt
+++ app/src/main/java/com/ihuntgore/recyclerherolab/view/fragments/HomeFragment.kt
@@ -21,6 +21,7 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(inflater)
         binding.lifecycleOwner = this
