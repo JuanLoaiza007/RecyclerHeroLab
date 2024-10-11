da89cc9b6435cd990e8ab070e339e22cd8538147
JuanLoaiza007 <juan.loaiza.santiago@correounivalle.edu.co>
2024-10-11 08:42:25 -0500

    refactor: create mvvm architecture

--- app/src/main/java/com/ihuntgore/recyclerherolab/view/MainActivity.kt
+++ app/src/main/java/com/ihuntgore/recyclerherolab/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.ihuntgore.recyclerherolab
+package com.ihuntgore.recyclerherolab.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ihuntgore.recyclerherolab.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
