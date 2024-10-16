5ba178b3ee48578338b352fd67a492364006a8de
JuanLoaiza007 <juan.loaiza.santiago@correounivalle.edu.co>
2024-10-16 08:13:16 -0500

    feat: implement navitagion from fragmentHome to fragmentRecycler

--- app/src/main/java/com/ihuntgore/recyclerherolab/view/fragments/HomeFragment.kt
+++ app/src/main/java/com/ihuntgore/recyclerherolab/view/fragments/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.ihuntgore.recyclerherolab.R
 import com.ihuntgore.recyclerherolab.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -26,5 +28,16 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(inflater)
         binding.lifecycleOwner = this
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navigateToRecycler()
+    }
+
+    private fun navigateToRecycler() {
+        binding.buttonFragmentRecycler.setOnClickListener{
+            findNavController().navigate(R.id.action_homeFragment_to_recyclerFragment)
+        }
     }
 }
