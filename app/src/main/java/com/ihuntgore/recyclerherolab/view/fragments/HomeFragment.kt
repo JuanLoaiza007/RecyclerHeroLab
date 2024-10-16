32ebf6b2eb18866ee63bf3e73a225062c0cbd651
JuanLoaiza007 <juan.loaiza.santiago@correounivalle.edu.co>
2024-10-16 09:14:13 -0500

    feat: implement permission request logic and openCamera() if granted

--- app/src/main/java/com/ihuntgore/recyclerherolab/view/fragments/HomeFragment.kt
+++ app/src/main/java/com/ihuntgore/recyclerherolab/view/fragments/HomeFragment.kt
@@ -1,10 +1,19 @@
 package com.ihuntgore.recyclerherolab.view.fragments
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.ihuntgore.recyclerherolab.R
 import com.ihuntgore.recyclerherolab.databinding.FragmentHomeBinding
@@ -12,6 +21,16 @@ import com.ihuntgore.recyclerherolab.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
+
+    // Guarda el resultado de una solicitud de permiso
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                openCamera()
+            } else {
+                Toast.makeText(context, "Necesitas aceptar los Permisos", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,11 +52,84 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navigateToRecycler()
+        addListenerCameraPermissions()
     }
 
     private fun navigateToRecycler() {
-        binding.buttonFragmentRecycler.setOnClickListener{
+        binding.buttonFragmentRecycler.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_recyclerFragment)
+        }
+    }
+
+    private fun addListenerCameraPermissions() {
+        binding.buttonCamera.setOnClickListener {
+            cameraPermitRequest()
+        }
+    }
+
+    private fun openCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, 0)
+    }
+
+    private fun cameraPermitRequest() {
+        // Para versiones de Android anteriores a 6.0 Marshmallow (API 23) no es necesario pedir este permiso
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            openCamera() // Abre la camara y ya está
+        }
+        // Para versiones de Android 6.0 Marshmallow (API 23) en adelante se debe solicitar el permiso
+        else {
+            when {
+
+                // Condición si ya se tiene el permiso
+                // checkSelfPermission(): Este método de la clase ContextCompat se usa para comprobar si se ha concedido un permiso específico a la aplicación.
+                // PackageManager.PERMISSION_GRANTED/PERMISSION_DENIED son valores para verificar si el permiso es concedido o no
+                ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED -> {
+                    openCamera()
+                }
+
+                // Condición si el permiso ha sido denegado previamente para volverlo a pedir explicando su importancia
+                // A esto se le llama hacer la solicitud racionalmente
+                shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA) -> {
+
+                    // Manualmente usaremos un AlertDialog antes de decirle al sistema que pida el permiso
+                    AlertDialog.Builder(requireContext())
+                        .setTitle("Permisos de Cámara")
+                        .setMessage("Los permisos de la camara son necesarios para que la aplicación funcione, esta dispuesto a acetarlos?")
+
+                        // Si el usuario nos dice que esta dispuesto, invocamos la funcion del sistema
+                        .setPositiveButton("Vamos!") { _, _ ->
+                            requestPermissionLauncher.launch(android.Manifest.permission.CAMERA)
+                        }
+
+                        // Si el usuario no esta dispuesto entonces no hacemos nada
+                        .setNegativeButton("Mejor no") { _, _ -> }.show()
+                }
+
+                // Condición si el permiso se ha denegado permantentemente, es decir,
+                // hicimos la solicitud racionalmente, nos rechazaron y ya no podemos solicitarlo desde la app
+                (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED &&
+                !shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA)) -> {
+                    AlertDialog.Builder(requireContext())
+                        .setTitle("Permisos de Cámara Denegados")
+                        .setMessage("El permiso de la cámara ha sido denegado permanentemente. Por favor, habilítelo manualmente desde la configuración de la aplicación.")
+
+                        // Si el usuario nos dice que esta dispuesto, lo redirigiremos a la configuracion de Android
+                        .setPositiveButton("Configuración") { _, _ ->
+                            val intent = Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS) // señal para abrir en Android la configuración de la app
+                            intent.data = Uri.parse("package:${requireContext().packageName}") // El nombre del paquete que usará la señal
+                            startActivity(intent) // Invocar la señal ya que está configurada
+                        }
+
+                        // Si el usuario no esta dispuesto pues ni modo
+                        .setNegativeButton("Cancelar") { dialog, _ -> dialog.dismiss() }.show()
+                }
+
+                // Condicion por defecto, pedir los permisos sin explicar pa que o que.
+                // Se ejecuta cuando la aplicación pide los permisos por primera vez porque no hay referencias de
+                // si el usuario lo ha aceptado o denegado.
+                else -> requestPermissionLauncher.launch(android.Manifest.permission.CAMERA)
+            }
         }
     }
 }
