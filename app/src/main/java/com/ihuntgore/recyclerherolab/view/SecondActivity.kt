e479d077b6fac407ef77b36b3a186d81b7f2415a
JuanLoaiza007 <juan.loaiza.santiago@correounivalle.edu.co>
2024-10-16 08:19:11 -0500

    feat: added SecondActivity

---
+++ app/src/main/java/com/ihuntgore/recyclerherolab/view/SecondActivity.kt
@@ -0,0 +1,12 @@
+package com.ihuntgore.recyclerherolab.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ihuntgore.recyclerherolab.R
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
